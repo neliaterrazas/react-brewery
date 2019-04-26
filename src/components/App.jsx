608dbdea55065d5af7e16f45e951350683a03dc1
Nelia Terrazas <terrazasnelia@gmail.com>
2019-04-26 13:33:45 -0700

    debug App.jsx in the render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Error from './Error';
 import Footer from './Footer';
 import Moment from 'moment';
 import Admin from '../components/admin/Admin';
+import BeerMenu from './BeerMenu';
 import CurrentTapList from '../components/admin/CurrentTapList';
 import BeerForm from '../components/admin/BeerForm';
 
@@ -15,16 +16,24 @@ class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      fixedBeer: fixedBeer,
     };
+    this.handleAddingNewBeer = this.handleAddingNewBeer.bind(this);
 
   }
+
+  handleAddingNewBeer(newBeer){
+    let updatedBeer = this.state.fixedBeer.slice();
+    updatedBeer.push(newBeer);
+    this.setState({fixedBeer: updatedBeer});
+  }
+
   render(){
     return (
       <div>
         <Header/>
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' render{() =><BeerMenu fixedBeer={this.state.fixedBeer} />} />
           <Route component={Error} />
         </Switch>
         <Footer/>
